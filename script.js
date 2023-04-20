42bf34f18c90ca3831a48369bb120f1d5404d0f3
chrille0234 <madsen.chr@hotmail.com>
2023-04-20 09:04:00 +0200

    fsdfs

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ var imageArray = [
         alt : 'placeholder image with a orange background and white text'
     },
     {
-        src : 'https://placehold.co/600x400/blue/white',
-        alt : 'placeholder image with a blue background and white text'
-    },
-    {
         src : 'https://placehold.co/600x400/green/white',
         alt : 'placeholder image with a green background and white text'
     },
     {
-        src : 'https://placehold.co/600x400/red/white',
-        alt : 'placeholder image with a red background and white text'
-    }
+        src : 'https://placehold.co/600x400/blue/white',
+        alt : 'placeholder image with a blue background and white text'
+    },
+    {
+        src : 'https://placehold.co/600x400/purple/white',
+        alt : 'placeholder image with a purple background and white text'
+    },
 ]
 MAIN_IMAGE.src = imageArray[0].src;
 MAIN_IMAGE.alt = imageArray[0].alt;
@@ -30,7 +30,12 @@ imageArray.forEach(function(image, index){
     img.addEventListener('mouseover', function(){
         MAIN_IMAGE.src = imageArray[index].src;
         MAIN_IMAGE.alt = imageArray[index].alt;
+        new ImageZoom(document.getElementById("img-container"), options);
+        var image_container = document.getElementById("img-container");
+        //sørger for at image-zoom-library ikke tilføjer en million divs til DOM'en. 
+        image_container.removeChild(image_container.children[1]);
+        image_container.removeChild(image_container.children[2]);
+        
+        
     })
 })
-
-
